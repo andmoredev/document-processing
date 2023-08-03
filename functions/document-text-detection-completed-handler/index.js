e850e19b29143888dc38815dc3fd96ed843d08ee
Andres Moreno <andrez.moreno.m@gmail.com>
2023-08-03 15:27:29 -0600

    get textract response

--- functions/document-text-detection-completed-handler/index.js
+++ functions/document-text-detection-completed-handler/index.js
@@ -2,7 +2,17 @@ const { initializePowertools } = process.env.LAMBDA_TASK_ROOT ?
   require('/opt/nodejs/lambda-powertools') :
   require('../../layers/lambda-powertools/lambda-powertools');
 
+const { TextractClient, GetDocumentTextDetectionCommand } = require('@aws-sdk/client-textract');
+const textractClient = new TextractClient();
+
 exports.handler = initializePowertools(async (event) => {
-  console.log('hello world');
+  const jobId = JSON.parse(event.Records[0].Sns.Message).JobId;
+  const command = new GetDocumentTextDetectionCommand({
+    JobId: jobId
+  });
+
+  const response = await textractClient.send(command);
+
+  console.log(JSON.stringify(response));
   return 'hello world';
 });
