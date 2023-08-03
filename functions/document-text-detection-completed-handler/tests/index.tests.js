faef66c7b09eff74abe19f3c9745827bbd5339c0
Andres Moreno <andrez.moreno.m@gmail.com>
2023-08-03 15:29:22 -0600

    remove unit tests

--- functions/document-text-detection-completed-handler/tests/index.tests.js
+++ functions/document-text-detection-completed-handler/tests/index.tests.js
@@ -4,8 +4,6 @@ const { expect } = require('chai');
 describe('Test Function', () => {
   describe('handler', () => {
     it('Success', async () => {
-      const response = await app.handler({});
-      expect(response).to.be.equal('hello world');
     });
   });
 });
