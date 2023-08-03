50c5adee58f69a2cbc2a8f168d8e7f27dd238f71
Andres Moreno <andrez.moreno.m@gmail.com>
2023-08-03 12:15:07 -0600

    rename layer

--- functions/document-text-detection-completed-handler/index.js
+++ functions/document-text-detection-completed-handler/index.js
@@ -1,6 +1,6 @@
 const { initializePowertools } = process.env.LAMBDA_TASK_ROOT ?
-  require('/opt/nodejs/lambda-powertools-utils') :
-  require('../../layers/lambda-powertools-utils/lambda-powertools-utils');
+  require('/opt/nodejs/lambda-powertools') :
+  require('../../layers/lambda-powertools/lambda-powertools');
 
 exports.handler = initializePowertools(async (event) => {
   console.log('hello world');
