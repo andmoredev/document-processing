69c7141d90b56358ef58e9b94c56b001cc05244c
Andres Moreno <andrez.moreno.m@gmail.com>
2023-08-03 14:56:33 -0600

    do start detection from lambda function

---
+++ functions/start-document-text-detection/index.js
@@ -0,0 +1,28 @@
+const { initializePowertools } = process.env.LAMBDA_TASK_ROOT ?
+  require('/opt/nodejs/lambda-powertools') :
+  require('../../layers/lambda-powertools/lambda-powertools');
+
+const { TextractClient, StartDocumentTextDetectionCommand } = require('@aws-sdk/client-textract');
+const textractClient = new TextractClient();
+
+exports.handler = initializePowertools(async (event) => {
+  const bucketName = event.detail.bucket.name;
+  const objectKey = event.detail.object.key;
+
+  const command = new StartDocumentTextDetectionCommand({
+    DocumentLocation: {
+      S3Object: {
+        Bucket: bucketName,
+        Name: objectKey
+      }
+    },
+    NotificationChannel: {
+      SNSTopicArn: process.env.DOCUMENT_TEXT_DETECTION_COMPLETED_SNS_TOPIC,
+      RoleArn: process.env.DOCUMENT_TEXT_DETECTION_COMPLETED_SNS_TOPIC_ROLE
+    }
+  });
+
+  const response = await textractClient.send(command);
+
+  return response.JobId;
+});
---
+++ functions/start-document-text-detection/tests/index.tests.js
@@ -0,0 +1,9 @@
+const app = require('../index.js');
+const { expect } = require('chai');
+
+describe('Test Function', () => {
+  describe('handler', () => {
+    it('Success', async () => {
+    });
+  });
+});
