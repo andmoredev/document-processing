353a527cb7ba696261c5f6dea3b9e8f05f032d28
Andres Moreno <andrez.moreno.m@gmail.com>
2023-08-22 17:39:04 -0600

    cleanup

--- functions/start-document-text-detection/tests/index.tests.js
+++
@@ -1,9 +0,0 @@
-const app = require('../index.js');
-const { expect } = require('chai');
-
-describe('Test Function', () => {
-  describe('handler', () => {
-    it('Success', async () => {
-    });
-  });
-});
